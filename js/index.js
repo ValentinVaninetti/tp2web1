064d9c0570a526773a3f6d2abd8defd04455b11b
ValentinVaninetti <valenga20@gmail.com>
2021-05-24 17:19:33 -0300

    Co-authored-by: NocheSolar <NocheSolar@users.noreply.github.com>

---
+++ js/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let btnVerify = document.querySelector("#btn_verify_captcha");
+let generate = document.querySelector("#generateCaptcha_btn");
+generate.addEventListener("click", CrearCaptcha);
+btnVerify.addEventListener("click", verificarCaptcha);
+let generatedCaptcha;
+
+
+function CrearCaptcha() {
+  let captcha = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 5; i++) {
+    captcha += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  document.querySelector("#captcha").innerHTML = captcha;
+  generatedCaptcha = captcha;
+}
+
+function verificarCaptcha() { 
+  event.preventDefault();
+  let input = document.querySelector("#input_captcha");
+  let respuesta = document.querySelector("#verificationResult") 
+  console.log(input.value == generatedCaptcha);  
+  if (input.value == generatedCaptcha) {   
+    respuesta.innerHTML = "Captcha correcto"
+    input.value = "";     
+    return true;        
+  } else {
+    respuesta.innerHTML = "Captcha incorrecto"
+    return false; 
+         
+  }
+}
+
