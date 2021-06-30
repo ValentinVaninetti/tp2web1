d127816dc73bc4f3f26df9ca3d84e31f2711bf2d
ValentinVaninetti <valenga20@gmail.com>
2021-06-30 16:47:37 -0300

    Sorting

--- js/newForo.js
+++ js/newForo.js
@@ -1,18 +1,33 @@
 "use strict"
 document.addEventListener("DOMContentLoaded",iniciarPagina);
-function iniciarPagina() {    
+function iniciarPagina() { 
+
     const url = 'https://60da1a6a5f7bf1001754794d.mockapi.io/api/Personajes';
+
     let botonAgregar = document.querySelector("#btn-agregar");
+
     botonAgregar.addEventListener("click", agregar);
+
     const tabla = document.querySelector("#table"); 
 
-   mostrarTabla();
-    async function mostrarTabla(){               
-        tabla.innerHTML = "";
-        try{
-            let res = await fetch(url);
-            let json = await res.json();
-            console.log(json);
+    let datosOriginales = []
+    
+   obtenerDatos();   
+    
+   async function obtenerDatos(){
+    try{
+        let res = await fetch(url);
+        let json = await res.json();
+        console.log(json);        
+        mostrarTabla(json);      
+        
+    }catch (error){
+        console.log("e");
+    }  
+
+   }
+    async function mostrarTabla(json){               
+        tabla.innerHTML = "";        
             for (const item of json){                
                 let nombre = item.nombrePersonaje;
                 let clase = item.clase;
@@ -22,53 +37,54 @@ function iniciarPagina() {
                 if (lvl == 60) {                    
                     tabla.innerHTML +=     
                         `<tr class="tablaResaltada"><td>${nombre}</td>
-                                <td>${clase}</td>
-                                <td>${raza}</td>
-                                <td>${especializacion}</td>
-                                <td>${lvl}</td>
-                                <td data-objectId="${item.id}">
-                                    <button 
-                                        class="buttonrow-Borrar"
-                                        type="button"                                        
-                                    >
-                                    Borrar
-                                    </button>
-                                    <button
-                                        class="buttonrow-Editar"
-                                        type="button"                                        
-                                    >Editar
-                                    </button>
-                                </td>                                
+                            <td>${clase}</td>
+                            <td>${raza}</td>
+                            <td>${especializacion}</td>
+                            <td>${lvl}</td>
+                            <td data-objectId="${item.id}">
+                                <button 
+                                    class="buttonrow-Borrar"
+                                    type="button"                                                                                                                        
+                                >
+                                Borrar
+                                </button>
+                                <button
+                                    class="buttonrow-Editar"
+                                    type="button"                                        
+                                >Editar
+                                </button>
+                            </td>                                
                         </tr>`
                  }
                 else {
                     tabla.innerHTML +=
                         `<tr><td>${nombre}</td>
-                                    <td>${clase}</td>
-                                    <td>${raza}</td>
-                                    <td>${especializacion}</td>
-                                    <td>${lvl}</td>
-                                    <td data-objectId="${item.id}"><button 
-                                        class="buttonrow-Borrar" 
-                                        type="button"                                       
-                                    >
-                                    Borrar
-                                     </button>                                     
-                                    <button
-                                        class="buttonrow-Editar"
-                                        type="button"                                        
-                                    >Editar
-                                    </button>
+                            <td>${clase}</td>
+                            <td>${raza}</td>
+                            <td>${especializacion}</td>
+                            <td>${lvl}</td>
+                            <td data-objectId="${item.id}">
+                                <button 
+                                    class="buttonrow-Borrar" 
+                                    type="button"                                                                               
+                                >
+                                Borrar
+                                    </button>                                     
+                                <button
+                                    class="buttonrow-Editar"
+                                    type="button"                                                                              
+                                >Editar
+                                </button>
                             </td>
                         </tr>`                      
-                }              
+                    }              
             }
             agregarEventoBorrar();
-            agregarEventoEditar();       
-        }catch (error){
-            console.log("e");
-        }  
-    }      
+            agregarEventoEditar(); 
+                  
+        
+    }
+
     async function agregar(event){
         event.preventDefault();
         let newPersonaje = {
@@ -90,46 +106,52 @@ function iniciarPagina() {
         }catch(error){
             console.log("e");            
        } 
-       mostrarTabla();
+       obtenerDatos();
     }
+
     function agregarEventoBorrar(){        
         let botonesBorrar = document.querySelectorAll(".buttonrow-Borrar");         
         botonesBorrar.forEach(boton => {            
             boton.addEventListener("click", borrar);            
             
         });
-    }        
+    }  
+
     async function borrar(event){
         //obtiene el atributo ligado al padre que contiene el boton Borrar que dispara el evento de esta funcion.
         //El padre tiene el id del Objeto referenciado en el atributo, en este caso data-objectID.
-        let id = event.target.parentNode.getAttribute("data-objectId");        
+        let id = event.target.parentNode.getAttribute("data-objectId");             
         console.log(id);
+
         try{
             let del = await fetch(`${url}/${id}`,{
                 'method' : 'DELETE'
-        });
+        }
+        );
+
         }catch(error){
                 console.log("error")
         }
-        mostrarTabla();
 
-            
+        obtenerDatos();                  
     }
+    
     function agregarEventoEditar(){
         let botonesEditar = document.querySelectorAll(".buttonrow-Editar");
         botonesEditar.forEach(boton =>{
             boton.addEventListener("click",editar);
-        })
-            
+        }) 
+
     }
-    async function editar(event){        
-        console.log("AA")
-        let id = event.target.parentNode.getAttribute("data-objectID");        
-        console.log(id);
+
+    async function editar(event){
+
+        let id = event.target.parentNode.getAttribute("data-objectID"); 
 
         let personajeSinModificar = getbyId(id);
 
         let PersonajeModificado = {
+
             nombrePersonaje: document.querySelector("#nombre").value,
             clase: document.querySelector("#clase").value,
             raza: document.querySelector("#raza").value,
@@ -162,9 +184,10 @@ function iniciarPagina() {
         catch(e){
             console.log(e)
         }
-        mostrarTabla();
+        obtenerDatos();
     }
-    //hacer de otra manera, pero anda.
+
+    //se puede hacer de otra manera, pero anda.
     async function getbyId(id){           
         try{
             let obj = await fetch(url + '/' + id);
@@ -173,9 +196,40 @@ function iniciarPagina() {
         }
         catch(e){
             console.log(e)
-        }
-        
+        }               
     }
+
+    let botonFiltro = document.querySelector("#btn-filtrar");
+    botonFiltro.addEventListener("click", filtrarBusqueda);
+    async function filtrarBusqueda (){
+        let filtro = document.querySelector("#filtroBusqueda").value;
+        let datosFiltrados = []
+        try{
+            let res = await fetch (url);
+            let json = await res.json ();
+            console.log ("aber");
+            tabla.innerHTML= "";
+            for (const personaje of json) {
+                console.log("x");
+                if ((filtro.toLowerCase() == personaje.nombrePersonaje.toLowerCase())
+                ||(filtro.toLowerCase()  == personaje.clase.toLowerCase())
+                ||(filtro.toLowerCase()  == personaje.raza.toLowerCase())
+                ||(filtro.toLowerCase()  == personaje.especializacion.toLowerCase())){
+                    console.log(personaje);
+                    datosFiltrados.push(personaje);                  
+                
+                }
+             if(!datosFiltrados.length == 0){
+             mostrarTabla(datosFiltrados);
+                }else{
+                    mostrarTabla(json)
+                }
+            }
+        }
+        catch (e){
+            console.log ("error")
+        }
+    }    
 }
 
 
